4f543b71d4bdcb002f3281b1a6b34e56574f05a0
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-07-25 04:01:21 +0530

    angular service improved to include search

--- frontend/app/scripts/services/twitterprovider.js
+++ frontend/app/scripts/services/twitterprovider.js
@@ -9,12 +9,12 @@
  */
 angular.module('twitterAppApp')
   .service('TwitterProvider', function ($http, $q, SERVER_URL) {
-      this.getTweets = function () {
-        var url = SERVER_URL + 'tweet';
+      this.getTweets = function (searchText) {
+        var url = SERVER_URL + 'tweet/getAnalyzedTweets/?search='+searchText;
         var deferred = $q.defer();
         $http.get(url).
             success(function (data, status, headers, config) {
-              deferred.resolve(data);
+              deferred.resolve(data.tweets);
             }).
             error(function (data, status, headers, config) {
               deferred.reject();
