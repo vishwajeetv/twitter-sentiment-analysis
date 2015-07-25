41bb20c12fcf96908e2b3656d9d4567f7b975c64
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-07-25 18:45:18 +0530

    created service for retrieving trends

--- twitter/api/services/TwitterService.js
+++ twitter/api/services/TwitterService.js
@@ -2,25 +2,26 @@
  * Created by vishwajeetv on 25/07/15.
  */
 
+
+var config = {
+    "consumer_key": sails.config.consumer_key,
+    "consumer_secret": sails.config.consumer_secret,
+    "access_token_key" : sails.config.access_token_key,
+    "access_token_secret" : sails.config.access_token_secret
+};
+
+var Twitter = require('twitter');
+var client = new Twitter(config);
+
+var Promise = require('bluebird');
+
 module.exports = {
 
 
-    getTweets: function(searchText) {
+    getTweets: function(query) {
 
-        var Promise = require('bluebird');
-
-        var config = {
-            "consumer_key": sails.config.consumer_key,
-            "consumer_secret": sails.config.consumer_secret,
-            "access_token_key" : sails.config.access_token_key,
-            "access_token_secret" : sails.config.access_token_secret
-        };
-
-        var Twitter = require('twitter');
-        var client = new Twitter(config);
-        
         //var params = { q: '#indvszim', count: 100};
-        var params = { q: searchText, count: 100};
+        var params = { q: query, count: 100};
 
         var tweets;
 
@@ -36,10 +37,23 @@ module.exports = {
                 }
             })
         });
+    },
+
+    getTrends : function(locationId){
+        return new Promise( function( resolve, reject )
+        {
+            var params = { id: locationId };
+            client.get('trends/place/', params, function(error, trends, response){
+                if (!error) {
+                    return resolve(trends);
+                }
+                else{
+                    console.log(error);
+                    return reject(error);
+                }
+            })
+        });
+}
 
 
-
-
-
-    }
 };
