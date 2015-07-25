bb7f5bc68ed042d18483e1249924cae2d8fb3329
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-07-25 23:17:58 +0530

    Font awesome added, smileys added for showing sentiment

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -23,6 +23,7 @@ angular.module('twitterAppApp')
         };
 
 
+        $scope.sentiment = null;
         $scope.search = function()
         {
             var searchText = $scope.searchText;
@@ -32,13 +33,43 @@ angular.module('twitterAppApp')
             {
                 $scope.sentimentalWordsAnalysis = tweetsData.sentimentalWordsAnalysis;
                 $scope.allWordsAnalysis = tweetsData.allWordsAnalysis;
-              $scope.numberOfTweets = tweetsData.overallAnalysis.numberOfTweets;
-            $scope.totalScore = tweetsData.overallAnalysis.totalScore;
+                $scope.numberOfTweets = tweetsData.overallAnalysis.numberOfTweets;
+                $scope.totalScore = tweetsData.overallAnalysis.totalScore;
                 $scope.averageScore = tweetsData.overallAnalysis.averageScore;
+                $scope.numberOfSentimentalTweets = tweetsData.overallAnalysis.numberOfSentimentalTweets;
+
+                var averageScore = $scope.averageScore;
+                $scope.sentiment = calculateSentiment(averageScore);
+
             }
         )
       }
 
+        function calculateSentiment(averageScore)
+        {
+            var sentiment = null;
+            var minimumSentimentIndex = 0.1
+            var highSentimentIndex = 0.80;
+            if(averageScore > minimumSentimentIndex){
+               sentiment = 'Positive';
+                if(averageScore > highSentimentIndex)
+                {
+                    sentiment = 'Very Positive';
+                }
+            }
+            else if( averageScore < -minimumSentimentIndex){
+               sentiment = 'Negative';
+                if(averageScore < -highSentimentIndex)
+                {
+                    sentiment = 'Very Negative';
+                }
+            }
+            else
+            {
+               sentiment = 'Neutral';
+            }
+            return sentiment;
+        }
         $scope.trends = {};
         $scope.getTrends = function(locationId)
         {
@@ -51,9 +82,24 @@ angular.module('twitterAppApp')
             )
         }
 
+        $scope.getSentimentClass = function()
+        {
+            if(($scope.sentiment == 'Positive') || ($scope.sentiment == 'Very Positive'))
+            {
+                return 'fa text-success fa-5x fa-smile-o'
+            }
+            else if(($scope.sentiment == 'Positive') || ($scope.sentiment == 'Very Positive'))
+            {
+                return 'fa text-danger fa-5x fa-frown-o'
+            }
+            else
+            {
+                return 'fa text-warning fa-5x fa-meh-o'
+            }
+        }
       function init()
       {
-            $scope.getTrends(1);
+            //$scope.getTrends(1);
       }
 
       init();
