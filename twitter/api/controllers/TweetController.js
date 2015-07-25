baae98be3b33e5ce6bbf9c614d67cec265de76a6
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-07-25 18:43:24 +0530

    Counting sentimental words service used, web service improved

--- twitter/api/controllers/TweetController.js
+++ twitter/api/controllers/TweetController.js
@@ -7,15 +7,67 @@
 
 module.exports = {
 
-    getAnalyzedTweets: function (req, res) {
+    getAnalyzedTweets: function (request , response )
+    {
+        var query = request.param('query');
 
-       tweets =  TwitterService.getTweets( req.param('search')).then(function(tweets){
-           return res.json({
-               tweets : SentimentAnalysisService.analyze(tweets)
+        var hasTweetsInDB = false;
+        var tweets = Tweet.find({'query':query},function(error, tweets) {
+        console.log(tweets.length);
+            if(tweets.length >= 1)
+            {
+                hasTweetsInDB = true;
+                getTweets();
+                return response.json({
+                    tweets: tweets,
+                    wordAnalysis : WordAnalysisService.countSentimentalWords(tweets)
+                });
+            }
+            else
+            {
+                getTweets();
+            }
+        });
+
+        var getTweets = function () {
+            TwitterService.getTweets(query).then(function(tweets){
+
+                tweets = SentimentAnalysisService.analyze(tweets);
+                tweets.forEach(function(tweet){
+                    tweet.query = query;
+                    Tweet.findOrCreate({id:tweet.id}, tweet).exec(function createFindCB(err, record){
+
+                    });
+
+                });
+                if(hasTweetsInDB == false) {
+                    return response.json({
+                        tweets: tweets,
+                        wordAnalysis : WordAnalysisService.countSentimentalWords(tweets)
+                    });
+                }
+            });
+        }
+
+
+    },
+
+    getTrends : function( request , response )
+    {
+        var locationId = request.param('location');
+        TwitterService.getTrends(locationId).then(function(trends) {
+           return response.json({
+               trends: trends
            });
-       });
+       },
+        function(error)
+        {
+            return response.json({
+                error: error
+            });
+        })
+    },
 
-    }
 
 };
 
