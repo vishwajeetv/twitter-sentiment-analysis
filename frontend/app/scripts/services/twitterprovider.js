baa5a03d7175cc9a2fe9c25905a3bef5a9659a81
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-07-25 18:44:31 +0530

    angular service updated for getting sentimental words

--- frontend/app/scripts/services/twitterprovider.js
+++ frontend/app/scripts/services/twitterprovider.js
@@ -10,11 +10,12 @@
 angular.module('twitterAppApp')
   .service('TwitterProvider', function ($http, $q, SERVER_URL) {
       this.getTweets = function (searchText) {
-        var url = SERVER_URL + 'tweet/getAnalyzedTweets/?search='+searchText;
+        var url = SERVER_URL + 'tweet/getAnalyzedTweets/?query='+searchText;
         var deferred = $q.defer();
         $http.get(url).
             success(function (data, status, headers, config) {
-              deferred.resolve(data.tweets);
+                console.log(data);
+              deferred.resolve(data);
             }).
             error(function (data, status, headers, config) {
               deferred.reject();
@@ -22,4 +23,18 @@ angular.module('twitterAppApp')
             });
         return deferred.promise;
       };
+
+        this.getTrends = function (locationId) {
+            var url = SERVER_URL + 'tweet/getTrends/?location='+locationId;
+            var deferred = $q.defer();
+            $http.get(url).
+                success(function (data, status, headers, config) {
+                    deferred.resolve(data.trends[0].trends);
+                }).
+                error(function (data, status, headers, config) {
+                    deferred.reject();
+                    console.log("could not retrieve tweets");
+                });
+            return deferred.promise;
+        };
   });
