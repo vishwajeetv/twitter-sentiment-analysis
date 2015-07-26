42962cdff3bf5f39fa651c82c60058081f8d68aa
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-07-26 18:57:32 +0530

    backend integration with firebase

--- twitter/api/controllers/TweetController.js
+++ twitter/api/controllers/TweetController.js
@@ -5,6 +5,19 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var config = {
+    "consumer_key": sails.config.consumer_key,
+    "consumer_secret": sails.config.consumer_secret,
+    "access_token_key" : sails.config.access_token_key,
+    "access_token_secret" : sails.config.access_token_secret
+};
+
+var Twitter = require('twitter');
+var client = new Twitter(config);
+var Firebase = require("firebase");
+var firebaseRef = new Firebase("https://twittersentiment.firebaseio.com/");
+
+
 module.exports = {
 
     getAnalyzedTweets: function (request , response )
@@ -18,20 +31,69 @@ module.exports = {
             {
                 hasTweetsInDB = true;
                 getTweets();
-                var sentimentalAnalysis =  WordAnalysisService.countSentimentalWords(tweets);
-                var overallAnalysis = SentimentAnalysisService.analyzeAll(tweets);
-                return response.json({
-                    overallAnalysis : overallAnalysis,
-                    sentimentalWordsAnalysis : sentimentalAnalysis,
-                    allWordsAnalysis : WordAnalysisService.countAllWords(tweets)
-                });
+                getStream();
+                processTweetsFromDB()
             }
             else
             {
+                getStream();
                 getTweets();
             }
         });
 
+        function processTweetsFromDB() {
+            Tweet.find({'query': query}, function (error, tweets) {
+                var sentimentalAnalysis = WordAnalysisService.countSentimentalWords(tweets);
+                var overallAnalysis = SentimentAnalysisService.analyzeAll(tweets);
+                var allWordAnalysis = WordAnalysisService.countAllWords(tweets);
+
+                var results = {
+                    overallAnalysis: overallAnalysis,
+                    sentimentalWordsAnalysis: sentimentalAnalysis,
+                    allWordsAnalysis: allWordAnalysis
+                };
+
+                var resultToStore = {};
+                 resultToStore[query] = results;
+
+                firebaseRef.set(
+                    resultToStore
+                );
+
+            })
+        }
+
+        var getStream = function(){
+
+            client.stream('statuses/filter', {track: query}, function(stream) {
+                var tweets = [];
+                stream.on('data', function(tweet) {
+                    tweets.push(tweet);
+                    console.log(tweet.text);
+                    if(tweets.length == 3)
+                    {
+                        saveTweets(tweets);
+                        tweets = [];
+                    }
+                });
+
+                stream.on('error', function(error) {
+                    throw error;
+                });
+            });
+
+            var saveTweets = function(tweets){
+                tweets = SentimentAnalysisService.analyze(tweets);
+                tweets.forEach(function(tweet){
+                    tweet.query = query;
+                    Tweet.findOrCreate({id:tweet.id}, tweet).exec(function createFindCB(err, record){
+
+                    });
+
+                });
+                processTweetsFromDB();
+            }
+            };
         var getTweets = function () {
             TwitterService.getTweets(query).then(function(tweets){
 
@@ -44,13 +106,25 @@ module.exports = {
 
                 });
                 if(hasTweetsInDB == false) {
+
                     var sentimentalAnalysis =  WordAnalysisService.countSentimentalWords(tweets);
                     var overallAnalysis = SentimentAnalysisService.analyzeAll(tweets);
-                    return response.json({
+                    var allWordAnalysis =  WordAnalysisService.countAllWords(tweets);
+
+                    var results = {
                         overallAnalysis : overallAnalysis,
                         sentimentalWordsAnalysis : sentimentalAnalysis,
-                        allWordsAnalysis : WordAnalysisService.countAllWords(tweets)
-                    });
+                        allWordsAnalysis : allWordAnalysis
+                    };
+
+                    var resultToStore = {};
+                    resultToStore[query] = results;
+
+                    firebaseRef.set(
+                        resultToStore
+                    );
+
+                    return response.json(results);
                 }
             });
         }
